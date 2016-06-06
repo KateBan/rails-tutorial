eb37e114564fd08c4f5082ff661be4e587dc8a0f
Katerina Baneva <kbaneva@gmail.com>
2016-06-06 21:40:09 +0300

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
 # Returns the current logged-in user (if any).
 # Returns the user corresponding to the remember token cookie.
 # Returns the user corresponding to the remember token cookie.
@@ -45,4 +50,14 @@ module SessionsHelper
     @current_user = nil
   end
 
+# Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
